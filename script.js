4b1742cbafba7f7e13b3a50eb2b35d9ee97bfc6f
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-16 15:43:20 +0500

    implement: calculator object functions, clearPanel add, sub etc

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 
 const PANEL = document.querySelector('#panel');
+const OPERATOR = document.querySelector('#operator');
+const NUMBER = document.querySelector('number');
 
-function clearPanel() {
-    PANEL.innerText = '';
+const ADDSYM = '&#43;';
+const SUBSYM = '&#8722;';
+const POWSYM = '&#94;';
+const DIVSYM = '&#247;';
+const MULSYM = '&#215;';
+
+function setOperator(operator) {
+    OPERATOR.innerHTML = operator;
+}
+
+const CALCULATOR = {
+    clearPanel: () => {
+        OPERATOR.innerHTML = '';
+        Number.innerText = '';
+    },
+    addition: () => {
+        setOperator(ADDSYM);
+    },
+    substraction: () => {
+        setOperator(SUBSYM);
+    },
+    power: () => {
+        setOperator(POWSYM);
+    },
+    division: () => {
+        setOperator(DIVSYM);
+    },
+    multiply: () => {
+        setOperator(MULSYM);
+    }
 }
